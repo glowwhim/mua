8acfb4dc4121cc22028baf5ce7361af6a2bc8678
glowwhim <62894858+glowwhim@users.noreply.github.com>
2020-07-26 15:11:00 +0800

    asd

--- machine/main.cpp
+++ machine/main.cpp
@@ -77,7 +77,7 @@ void run_mua()
     fclose(rfile);
 
     int cmd_address = 0;
-    int stack_address = 0;
+    int stack_top = 0;
     unsigned char cmd;
     unsigned char thread_stack[1024];
 
@@ -91,27 +91,27 @@ void run_mua()
         //printf("%d: %d\n", cmd_address-1, cmd);
         if (cmd == CMD_PUSH_INT || cmd == CMD_PUSH_FLOAT)
         {
-            memcpy(thread_stack + stack_address, mua + cmd_address, 4);
+            memcpy(thread_stack + stack_top, mua + cmd_address, 4);
             cmd_address += 4;
-            stack_address += 4;
+            stack_top += 4;
         }
         else if (cmd == CMD_PUSH_SEGMENT_INT)
         {
             temp_int = (int*) (mua + cmd_address);
             unsigned char *int2 = thread_stack + temp_int[0];
             int i = *(int*)int2;
-            //printf("push setgment int from %d to %d = %d\n", temp_int[0], stack_address, i);
-            memcpy(thread_stack + stack_address, &i, 4);
+            //printf("push setgment int from %d to %d = %d\n", temp_int[0], stack_top, i);
+            memcpy(thread_stack + stack_top, &i, 4);
             cmd_address += 4;
-            stack_address += 4;
+            stack_top += 4;
         }
         else if (cmd == CMD_FJ)
         {
-            temp_char = (char*) (thread_stack + stack_address - 1);
+            temp_char = (char*) (thread_stack + stack_top - 1);
             temp_int = (int*) (mua + cmd_address);
             if (temp_char[0]) cmd_address += 4;
             else cmd_address = *temp_int;
-            stack_address -= 1;
+            stack_top -= 1;
         }
         else if (cmd == CMD_JUMP)
         {
@@ -121,83 +121,83 @@ void run_mua()
         else if (cmd == CMD_PUSH_SEGMENT_FLOAT)
         {
             temp_int = (int*) (mua + cmd_address);
-            memcpy(thread_stack + stack_address, thread_stack + temp_int[0], 4);
+            memcpy(thread_stack + stack_top, thread_stack + temp_int[0], 4);
             cmd_address += 4;
-            stack_address += 4;
+            stack_top += 4;
         }
         else if (cmd == CMD_MUL_INT_INT)
         {
-            temp_int = (int*) (thread_stack + stack_address - 8);
+            temp_int = (int*) (thread_stack + stack_top - 8);
             temp_int[0] = temp_int[0] * temp_int[1];
-            stack_address -= 4;
+            stack_top -= 4;
         }
         else if (cmd == CMD_ADD_INT_INT)
         {
-            temp_int = (int*) (thread_stack + stack_address - 8);
+            temp_int = (int*) (thread_stack + stack_top - 8);
             //printf("%d + %d = %d\n", temp_int[0], temp_int[1], temp_int[0] + temp_int[1]);
             temp_int[0] = temp_int[0] + temp_int[1];
-            stack_address -= 4;
+            stack_top -= 4;
         }
         else if (cmd == CMD_MUL_INT_FLOAT)
         {
-            temp_int = (int*) (thread_stack + stack_address - 8);
-            temp_float = (float*) (thread_stack + stack_address - 8);
+            temp_int = (int*) (thread_stack + stack_top - 8);
+            temp_float = (float*) (thread_stack + stack_top - 8);
             temp_float[0] = temp_int[0] * temp_float[1];
-            stack_address -= 4;
+            stack_top -= 4;
         }
         else if (cmd == CMD_ADD_INT_FLOAT)
         {
-            temp_int = (int*) (thread_stack + stack_address - 8);
-            temp_float = (float*) (thread_stack + stack_address - 8);
+            temp_int = (int*) (thread_stack + stack_top - 8);
+            temp_float = (float*) (thread_stack + stack_top - 8);
             temp_float[0] = temp_int[0] + temp_float[1];
-            stack_address -= 4;
+            stack_top -= 4;
         }
         else if (cmd == CMD_LT_INT_FLOAT)
         {
-            temp_char = (char*) (thread_stack + stack_address - 8);
-            temp_int = (int*) (thread_stack + stack_address - 8);
-            temp_float = (float*) (thread_stack + stack_address - 8);
+            temp_char = (char*) (thread_stack + stack_top - 8);
+            temp_int = (int*) (thread_stack + stack_top - 8);
+            temp_float = (float*) (thread_stack + stack_top - 8);
             temp_char[0] = temp_int[0] < temp_float[1];
-            stack_address -= 7;
+            stack_top -= 7;
         }
         else if (cmd == CMD_LT_INT_INT)
         {
-            temp_char = (char*) (thread_stack + stack_address - 8);
-            temp_int = (int*) (thread_stack + stack_address - 8);
+            temp_char = (char*) (thread_stack + stack_top - 8);
+            temp_int = (int*) (thread_stack + stack_top - 8);
             temp_char[0] = temp_int[0] < temp_int[1];
             //printf("%d < %d = %d\n", temp_int[0], temp_int[1], temp_char[0]);
-            stack_address -= 7;
+            stack_top -= 7;
         }
         else if (cmd == CMD_SET_FLOAT_FLOAT)
         {
-            temp_int = (int*) (thread_stack + stack_address - 4);
-            temp_float = (float*) (thread_stack + stack_address - 8);
+            temp_int = (int*) (thread_stack + stack_top - 4);
+            temp_float = (float*) (thread_stack + stack_top - 8);
             ((float*) (thread_stack + *temp_int))[0] = temp_float[0];
-            stack_address -= 8;
+            stack_top -= 8;
         }
         else if (cmd == CMD_SET_INT_INT)
         {
-            temp_int = (int*) (thread_stack + stack_address - 8);
+            temp_int = (int*) (thread_stack + stack_top - 8);
             int i = temp_int[0];
             int j = temp_int[1];
             //printf("set %d to %d\n", i, j);
             ((int*) (thread_stack + j))[0] = i;
-            stack_address -= 8;
+            stack_top -= 8;
         }
         else if (cmd == CMD_PRINT_CHAR)
         {
-            printf("%d\n", *((char*) (thread_stack + stack_address - 1)));
-            stack_address -= 1;
+            printf("%d\n", *((char*) (thread_stack + stack_top - 1)));
+            stack_top -= 1;
         }
         else if (cmd == CMD_PRINT_INT)
         {
-            printf("%d\n", *((int*) (thread_stack + stack_address - 4)));
-            stack_address -= 4;
+            printf("%d\n", *((int*) (thread_stack + stack_top - 4)));
+            stack_top -= 4;
         }
         else if (cmd == CMD_PRINT_FLOAT)
         {
-            printf("%lf\n", *((float*) (thread_stack + stack_address - 4)));
-            stack_address -= 4;
+            printf("%lf\n", *((float*) (thread_stack + stack_top - 4)));
+            stack_top -= 4;
         }
         else if (cmd == CMD_EXIT)
         {
@@ -208,7 +208,7 @@ void run_mua()
             printf("unknown cmd %d\n", cmd);
             break;
         }
-        //print_stack(thread_stack, stack_address);
+        //print_stack(thread_stack, stack_top);
     }
     free(mua);
 }
