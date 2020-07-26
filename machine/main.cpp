cfea0b782e41afb54845943ee28e6e273420dbd8
glowwhim <62894858+glowwhim@users.noreply.github.com>
2020-07-26 10:49:57 +0800

    asd

--- machine/main.cpp
+++ machine/main.cpp
@@ -20,7 +20,7 @@ void gen_mua()
     {
         c = (unsigned char) cmd;
         fwrite(&c, 1, 1, wfile);
-        if (cmd == CMD_PUSH_INT)
+        if (cmd == CMD_PUSH_INT || cmd == CMD_PUSH_SEGMENT_FLOAT)
         {
             fscanf(rfile, "%d", &d1);
             fwrite(&d1, 4, 1, wfile);
@@ -33,10 +33,10 @@ void gen_mua()
         else if (cmd == CMD_MUL_INT_INT 
             || cmd == CMD_ADD_INT_INT 
             || cmd == CMD_MUL_INT_FLOAT 
+            || cmd == CMD_SET_FLOAT_FLOAT
             || cmd == CMD_ADD_INT_FLOAT 
             || cmd == CMD_PRINT_CHAR
             || cmd == CMD_PRINT_FLOAT
-            || cmd == CMD_PRINT_ADDRESS
             || cmd == CMD_EXIT)
         {
             continue;
@@ -73,15 +73,17 @@ void run_mua()
     while (cmd_address < mua_size)
     {
         cmd = mua[cmd_address++];
-        if (cmd == CMD_PUSH_INT)
+        if (cmd == CMD_PUSH_INT || cmd == CMD_PUSH_FLOAT)
         {
             memcpy(thread_stack + stack_address, mua + cmd_address, 4);
             cmd_address += 4;
             stack_address += 4;
         }
-        else if (cmd == CMD_PUSH_FLOAT)
+        else if (cmd == CMD_PUSH_SEGMENT_FLOAT)
         {
-            memcpy(thread_stack + stack_address, mua + cmd_address, 4);
+            temp_int = (int*) (mua + cmd_address);
+            temp_float = (float*) (thread_stack + temp_int[0]);
+            memcpy(thread_stack + stack_address, thread_stack + temp_int[0], 4);
             cmd_address += 4;
             stack_address += 4;
         }
@@ -110,6 +112,13 @@ void run_mua()
             temp_float = (float*) (thread_stack + stack_address - 8);
             temp_float[0] = temp_int[0] + temp_float[1];
             stack_address -= 4;
+        }
+        else if (cmd == CMD_SET_FLOAT_FLOAT)
+        {
+            temp_int = (int*) (thread_stack + stack_address - 4);
+            temp_float = (float*) (thread_stack + stack_address - 8);
+            ((float*) (thread_stack + *temp_int))[0] = temp_float[0];
+            stack_address -= 8;
         }
         else if (cmd == CMD_PRINT_INT)
         {
